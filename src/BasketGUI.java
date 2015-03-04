4593fcd38fd1af62a65dc44d794ee66ecafe05fc
Trent Holliday <tholliday@morristechnology.com>
2015-03-04 16:18:33 -0500

    Adding fields to allow for the creation of new customers in the database

--- src/BasketGUI.java
+++ src/BasketGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,26 +15,92 @@ public class BasketGUI {
     private JPanel inventoryTab;
     private JPanel orderTab;
     private JList inventoryList;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField emailField;
+    private JTextField addressField;
+    private JTextField stateField;
+    private JTextField countryField;
+    private JPasswordField passwordField;
+    private JButton addCustomerButton;
+    private JList customerList;
+    private JTextField customerField;
+    private JTextField cityField;
 
     public BasketGUI() {
 
+        addCustomerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                addCustomer();
+                reloadCustomerList();
+            }
+        });
+    }
+
+    private static ResultSet getResultsFromQuery(String sqlQuery){
+        DefaultListModel listModel = new DefaultListModel();
+        java.sql.Connection connection = Connection.getConnection();
+        ResultSet results;
+        try {
+            Statement statement = connection.createStatement();
+            results = statement.executeQuery(sqlQuery);
+        }catch(SQLException ex){
+            System.out.println(ex);
+            results = null;
+        }
+        return results;
+    }
+
+    private void addCustomer(){
+        String updateQuery = "insert into customers (customerid, firstname, lastname, password)" +
+                                " values ("+
+                                    "'" + customerField.getText() +
+                                    "', '" + firstNameField.getText() +
+                                    "', '" + lastNameField.getText() +
+                                    "', '" + passwordField.getText() +
+                                "')";
+        getResultsFromQuery(updateQuery);
+    }
+
+    private void reloadCustomerList(){
+        ResultSet customerResults = getResultsFromQuery("select * from customers");
+        DefaultListModel customerModel = new DefaultListModel();
+        try {
+            while (customerResults.next())
+                customerModel.addElement(customerResults.getString("firstname") + " " + customerResults.getString("lastname"));
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        customerList.setModel(customerModel);
     }
 
     public static void main(String[] args) {
-        java.sql.Connection connection = Connection.getConnection();
         BasketGUI gui = new BasketGUI();
         JFrame frame = new JFrame("BasketGUI");
         frame.setContentPane(gui.panel1);
-        DefaultListModel listModel = new DefaultListModel();
+        ResultSet inventoryResults = getResultsFromQuery("select * from item");
+
+        DefaultListModel inventoryModel = new DefaultListModel();
+
         try {
-            Statement invStatement = connection.createStatement();
-            ResultSet invResults = invStatement.executeQuery("select * from item");
-            while(invResults.next())
-                listModel.addElement(invResults.getString("itemname") + " - $" + invResults.getDouble("price"));
-        }catch(SQLException ex){
-            System.out.println(ex);
+            while (inventoryResults.next())
+                inventoryModel.addElement(inventoryResults.getString("itemname") + " - $" + inventoryResults.getDouble("price"));
+
+        }catch(SQLException e){
+            e.printStackTrace();
         }
-        gui.inventoryList.setModel(listModel);
+        gui.inventoryList.setModel(inventoryModel);
+
+        ResultSet customerResults = getResultsFromQuery("select * from customers");
+        DefaultListModel customerModel = new DefaultListModel();
+        try {
+            while (customerResults.next())
+                customerModel.addElement(customerResults.getString("firstname") + " " + customerResults.getString("lastname"));
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        gui.customerList.setModel(customerModel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
